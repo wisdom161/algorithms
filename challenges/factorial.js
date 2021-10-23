cf3f85e55ea37c1c28c4ed0b6dcf53dabd1ccd7f
Wisdom Liu <wisdom.code161@gmail.com>
2021-10-23 12:37:03 -0700

    Added new factorial solution

--- challenges/factorial.js
+++ challenges/factorial.js
@@ -1,4 +1,4 @@
-// write a factorial function >> 5! = 5*4*3*2*1 = 120
+// // write a factorial function >> 5! = 5*4*3*2*1 = 120
 
 function factorial(n) {
   let f = Array(n);
@@ -12,11 +12,33 @@ function factorial(n) {
 
 console.log(factorial(5))
 
-// factorial(5) >> f[5] = factorial(4) * 5
-// factorial(4) >> f[4] = factorial(3) * 4
-// factorial(3) >> f[3] = factorial(2) * 3
-// factorial(2) >> f[2] = factorial(1) * 2
-// factorial(2) >> f[2] = 1 * 2 >> 2
-// factorial(3) >> f[3] = 2 * 3 >> 6
-// factorial(4) >> f[4] = 6 * 4 >> 24
-// factorial(5) >> f[5] = 24 * 5 >> 120
+// // factorial(5) >> f[5] = factorial(4) * 5
+// // factorial(4) >> f[4] = factorial(3) * 4
+// // factorial(3) >> f[3] = factorial(2) * 3
+// // factorial(2) >> f[2] = factorial(1) * 2
+// // factorial(2) >> f[2] = 1 * 2 >> 2
+// // factorial(3) >> f[3] = 2 * 3 >> 6
+// // factorial(4) >> f[4] = 6 * 4 >> 24
+// // factorial(5) >> f[5] = 24 * 5 >> 120
+
+// const factorial = (n) => {
+
+//   // permutations often use factorial logic
+//   // eg.) The number of ways of seating 6 students in a row of 6 chairs is:
+
+//   if (n === 0 || n === 1) return 1
+  
+//   return n * factorial(n - 1)
+// }
+
+// console.log(factorial(5))
+// // factorial(5) >> 5 * factorial(4)
+// // factorial(4) >> 4 * factorial(3)
+// // factorial(3) >> 3 * factorial(2)
+// // factorial(2) >> 2 * factorial(1)
+// // factorial(1) >> 1 * 1
+// // factorial(1) >> 1
+// // factorial(2) >> 2 * 1 >> 2
+// // factorial(3) >> 3 * 2 >> 6
+// // factorial(4) >> 4 * 6 >> 24
+// // factorial(5) >> 5 * 24 >> 120
